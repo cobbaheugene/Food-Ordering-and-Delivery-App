efccf9a8093f684dc52ab18b874543d15f388959
qbentil <Bentilshadrack72@gmail.com>
2022-04-06 18:20:12 +0000

    Update✔: Update fedtch menu func

--- functions/processor.php
+++ functions/processor.php
@@ -1,15 +1,20 @@
 <?php
-//we begin by defining database connection details
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "SWD";//create a database called SWD in phymyadmin if you have not done so
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);//connecting to the database with specified credentials
-// Check connection
-if ($conn->connect_error) {//checking if there are errors
-  die("Connection failed: " . $conn->connect_error);//Display this if there are errors
+function get_menu()
+{
+  //we begin by defining database connection details
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "SWD";//create a database called SWD in phymyadmin if you have not done so
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);//connecting to the database with specified credentials
+  // Check connection
+  if ($conn->connect_error) {//checking if there are errors
+    die("Connection failed: " . $conn->connect_error);//Display this if there are errors
+  }
+  $sql = "SELECT Firstname, Lastname, Email FROM student";//we want to select all students Firstname,Lastname and email
+  $result = $conn->query($sql);//put the query($sql) results in a variable called $result
 }
-
 ?>
