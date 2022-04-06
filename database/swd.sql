319a165e402a22dc03fa2f79edf9ac16ae5d8ad5
qbentil <Bentilshadrack72@gmail.com>
2022-04-06 22:40:32 +0000

    Update databasse structure

--- database/swd.sql
+++ database/swd.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 06, 2022 at 06:42 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.28
+-- Generation Time: Apr 07, 2022 at 12:38 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `name` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `message`, `name`) VALUES
+(3, 'I really love your customer services and swift way of delivery', 'John Boadu'),
+(4, 'Working on a super fantastic admin control panel. You gotta watch this pace', 'Cobbah Eugene');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_food`
 --
 
@@ -31,11 +51,19 @@ CREATE TABLE `tbl_food` (
   `id` int(11) NOT NULL,
   `name` varchar(225) NOT NULL,
   `description` text NOT NULL,
-  `price` decimal(10,0) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
   `rating` int(11) NOT NULL,
   `img_url` text DEFAULT 'food.jpg',
   `date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_food`
+--
+
+INSERT INTO `tbl_food` (`id`, `name`, `description`, `price`, `rating`, `img_url`, `date`) VALUES
+(1, 'Margherita Pizza', 'Lorem Ipsum Dolor Sit, Amet Consectetur Adipisicing Elit. Excepturi, Accusantium.', '42.99', 4, 'menu-1.jpg', '2022-04-06 16:56:30'),
+(2, 'Stuffed Bean Burger', 'Lorem Ipsum Dolor Sit, Amet Consectetur Adipisicing Elit. Excepturi, Accusantium.', '72.99', 4, 'menu-2.jpg', '2022-04-06 16:57:53');
 
 -- --------------------------------------------------------
 
@@ -58,21 +86,33 @@ CREATE TABLE `tbl_login` (
 
 CREATE TABLE `tbl_order` (
   `id` int(11) NOT NULL,
-  `food` varchar(50) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
+  `food_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `total` decimal(10,2) NOT NULL,
-  `order _date` datetime NOT NULL,
+  `order_date` datetime NOT NULL DEFAULT current_timestamp(),
   `status` varchar(50) NOT NULL,
   `customer_name` varchar(50) NOT NULL,
   `customer_contact` varchar(20) NOT NULL,
   `customer_email` varchar(50) NOT NULL,
-  `customer_address` varchar(50) NOT NULL
+  `customer_address` text NOT NULL,
+  `message` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_order`
+--
+
+INSERT INTO `tbl_order` (`id`, `food_id`, `quantity`, `order_date`, `status`, `customer_name`, `customer_contact`, `customer_email`, `customer_address`, `message`) VALUES
+(2, 2, 3, '2022-04-07 12:24:12', 'processing', 'Cobbah Eugene', '1234567890', '1234567890', 'Legon\r\nMain Hall', 'I want it now');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tbl_food`
@@ -91,16 +131,22 @@ ALTER TABLE `tbl_order`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `tbl_food`
 --
 ALTER TABLE `tbl_food`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_order`
 --
 ALTER TABLE `tbl_order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
